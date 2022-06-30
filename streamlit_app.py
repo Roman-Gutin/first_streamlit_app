b9418dc86ecc1bf09015137bc06824adfd0af844
Roman-Gutin <48274547+Roman-Gutin@users.noreply.github.com>
2022-06-30 14:54:03 -0400

    Update streamlit_app.py
    list

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,5 @@ import pandas as pd
 streamlit.title('LETF Spread Dashboard')
 
 fruits = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+streamlit.multiselect('pick fruits or die:', list(fruits.index))
 streamlit.dataframe(fruits)

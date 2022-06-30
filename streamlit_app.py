812fc818e7bb62f854f28467a7649e5673815b7a
Roman-Gutin <48274547+Roman-Gutin@users.noreply.github.com>
2022-06-30 14:52:31 -0400

    Update streamlit_app.py
    pandas and fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 
 import streamlit
-
+import pandas as pd
 streamlit.title('LETF Spread Dashboard')
+
+fruits = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+streamlit.dataframe(fruits)

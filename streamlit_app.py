9fc1fb0da02e2f24cec527b9d6f5a8476fea48f0
Roman-Gutin <48274547+Roman-Gutin@users.noreply.github.com>
2022-08-08 15:13:15 -0400

    text_input

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,8 @@ streamlit.title('LETF Spread Dashboard')
 fruits = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 streamlit.multiselect('pick fruits or die:', list(fruits.index))
 streamlit.dataframe(fruits)
-fruit = 'kiwi'
+
+fruit = streamlit.text_input('What fruit tickles your fancy?', 'Kiwi')
 fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit}")
 streamlit.text(fruityvice_response.json())
 # write your own comment -what does the next line do? 

ed42076c6ca91d67a7506dbf3e2fbdde42d6a7f5
Roman-Gutin <48274547+Roman-Gutin@users.noreply.github.com>
2022-08-08 16:57:20 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,17 +7,24 @@ from urllib.error import URLError
 
 streamlit.title('LETF Spread Dashboard')
 
+def get_fruity_vice_data(this_fruit_choice):
+    response = requests.get(f"https://fruityvice.com/api/fruit/{this_fruit_choice}")
+    return pd.json_normalize(response.json())
+  
 fruits = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 streamlit.multiselect('pick fruits or die:', list(fruits.index))
 streamlit.dataframe(fruits)
-
-fruit = streamlit.text_input('What fruit tickles your fancy?', 'Kiwi')
-fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit}")
-streamlit.text(fruityvice_response.json())
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+try:
+  
+  fruit_choice = streamlit.text_input('What fruit tickles your fancy?', 'Kiwi')
+  if not fruit_choice:
+    streamlit.error('Please select a fruit to get information.')
+   else:
+    data= get_fruity_vice_data(fruit_choice)
+    streamlit.dataframe(data)
+except URLErorr as e:
+  streamlit.error()
+streamlit.stop()    
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
@@ -26,5 +33,5 @@ streamlit.text("The Fruit Load List:")
 streamlit.dataframe(my_data_row)
 add_my_fruit = streamlit.text_input('What fruit tickles your fancy Part 2?', 'jackfruit')
 my_cur.execcute("insert into fruit_load_list values ('from streamlist')")
-streamlit.stop()
+
               

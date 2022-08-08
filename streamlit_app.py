836249aa69537066fae9f7a2068e693f997567ac
Roman-Gutin <48274547+Roman-Gutin@users.noreply.github.com>
2022-08-08 15:05:41 -0400

    more poop

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,8 @@ streamlit.text(fruityvice_response.json())
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
+txt = streamlit.text(str(-1))
 while 1:
   for i in range(10):
-    streamlit.text(str(i))
+    txt = streamlit.text(str(1))
 

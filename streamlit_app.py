3160c0e1b69f00789f00be96a4364337ea614a02
Roman-Gutin <48274547+Roman-Gutin@users.noreply.github.com>
2022-08-08 15:01:46 -0400

    Update streamlit_app.py
    api call

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,13 @@
 
 import streamlit
 import pandas as pd
+import requests
+
 streamlit.title('LETF Spread Dashboard')
 
 fruits = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 streamlit.multiselect('pick fruits or die:', list(fruits.index))
 streamlit.dataframe(fruits)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
